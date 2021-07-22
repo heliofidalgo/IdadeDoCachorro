76bd11b23d49672864e3af0917dce5f20e3113e8
Udemy Curse Swift <hmnff@hotmail.com>
2021-07-22 16:48:05 +0100

    Aviso de bug
    O app precisa atualizar o codigo que possui um bug.

--- IdadeDoCachorro/ViewController.swift
+++ IdadeDoCachorro/ViewController.swift
@@ -8,7 +8,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var campoIdadePer: UITextField!
+    @IBOutlet weak var legendaResultado: UILabel!
+    
+    
+    @IBAction func descobrirIdade(_ sender: Any) {
+        
+        if (campoIdadePer != nil) {
+            
+            let idade = Int(campoIdadePer.text!)! * 7
+            legendaResultado.text = "A idade do PET é: " + String(idade)
+        } else{
+            _ = Int(campoIdadePer.text!)! * 0
+            legendaResultado.text = "A idade do PET não foi digitada, insira um número válido! "
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
